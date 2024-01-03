3e79b2b1620949a8e9355afc6d4b60f4dd7c0d3b
siro <zhengchengshanqi@gmail.com>
2024-01-03 04:21:02 -0800

    yt

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -133,7 +133,7 @@ function App() {
                 onClick={qstartBtn}
               >
                 <img className="kaki-img" src="/images/pen.png" alt="" />
-                <h2 className="item">━ 意外と書けない漢字編 練習字合━</h2>
+                <h2 className="item">━ 意外と書けない漢字編 <ruby>練習字合 <rt>れんしゅうじあい</rt></ruby> ━</h2>
                 {/* <h3 style={displayStyle}>CLEAR!</h3> */}
               </div>
               {/* 漢伐↓ */}
@@ -142,7 +142,7 @@ function App() {
                 onClick={qstartBtn2}
               >
                 <img className="kaki-img" src="/images/penwhite.png" alt="" />
-                <h2 className="item">━ 意外と書けない漢字編 漢伐━</h2>
+                <h2 className="item">━ 意外と書けない漢字編 <ruby>答伐<rt>とうばつ</rt></ruby> ━</h2>
                 <h3 style={displayStyle}>CLEAR!</h3>
               </div>
               {/* ストーリー↓ */}
